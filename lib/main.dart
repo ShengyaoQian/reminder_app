5a62678e3010dba3f27106d24942637cab6c82d2
ShengyaoQian <qianshengyao13@gmail.com>
2021-11-13 09:45:15 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
         home: const MyHomePage(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
